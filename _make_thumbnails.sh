16785e92c06de6da6ef8fd2c1d4f54e9cf946819
Ketan13294 <mketan92@gmail.com>
2026-02-13 15:29:30 -0500

    Fixed the thumbnail making script for linux and generated the images.

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,7 @@
-for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
+for file in images/*.jpg images/*.png; do
+    # Skip if there are no matching files (e.g., if you only have jpgs and no pngs)
+    [ -e "$file" ] || continue
+    
+    # Generate the thumbnail, grabbing only the first frame/layer
+    [ ! -f "tn/$file" ] && convert "${file}[0]" -thumbnail 160x160 "tn/$file"
 done
